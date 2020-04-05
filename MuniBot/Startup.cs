bcd57172af1f89c115e1fe9b7b267c74e7f3d91a
rchavez <U201517696@upc.edu.pe>
2020-04-04 20:40:33 -0500

    Configurar Azure Blob Storage

--- MuniBot/Startup.cs
+++ MuniBot/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Azure;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -25,6 +26,17 @@ namespace MuniBot
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            var storage = new AzureBlobStorage(
+                Configuration.GetSection("StorageConnectionString").Value,
+                Configuration.GetSection("StorageContainer").Value
+                );
+            var userState = new UserState(storage);
+            services.AddSingleton(userState);
+
+            var conversationState = new ConversationState(storage);
+            services.AddSingleton(conversationState);
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
             // Create the Bot Framework Adapter with error handling enabled.
