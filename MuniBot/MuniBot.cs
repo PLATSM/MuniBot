39806e9717ab9230df4156b7ef65da27187ff144
rchavez <U201517696@upc.edu.pe>
2020-04-07 19:30:00 -0500

    Cambios es el mensaje de bienvenida

--- MuniBot/MuniBot.cs
+++ MuniBot/MuniBot.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
@@ -11,6 +12,7 @@ using Microsoft.Bot.Schema;
 using Microsoft.EntityFrameworkCore;
 using MuniBot.Common.Models.User;
 using MuniBot.Data;
+using Newtonsoft.Json;
 
 namespace MuniBot
 {
@@ -20,6 +22,8 @@ namespace MuniBot
         private readonly BotState _conversationState;
         private readonly Dialog _dialog;
         private readonly IDataBaseService _databaseService;
+
+        private const string WelcomeMessage = "Bienvenido al asistente virtual de la Municipalidad!";
 
         public MuniBot(UserState userState,ConversationState conversationState, T dialog, IDataBaseService databaseService) {
             _userState = userState;
@@ -33,9 +37,68 @@ namespace MuniBot
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
                 {
-                    await turnContext.SendActivityAsync(MessageFactory.Text($"Bienvenido al asistente virtual de la Municipalidad!"), cancellationToken);
+                    // await turnContext.SendActivityAsync(MessageFactory.Text($"Bienvenido al asistente virtual de la Municipalidad!"), cancellationToken);
+                    // await turnContext.SendActivityAsync(WelcomeMessage, cancellationToken: cancellationToken);
+
+                    var welcomeCard = CreateWelcomeCard();
+                    var responseWelcome = MessageFactory.Attachment(welcomeCard, ssml: "Bienvenido!");
+                    await turnContext.SendActivityAsync(responseWelcome, cancellationToken);
+                    await _dialog.RunAsync(turnContext, _conversationState.CreateProperty<DialogState>("DialogState"), cancellationToken);
+                    await Task.Delay(500);
+
+                    //var menuCard = CreateMenuCard();
+                    //var responseMenu = MessageFactory.Attachment(menuCard, ssml: "Menu");
+                    //await turnContext.SendActivityAsync(responseMenu, cancellationToken);
+                    //await _dialog.RunAsync(turnContext, _conversationState.CreateProperty<DialogState>("DialogState"), cancellationToken);
+                    //await Task.Delay(500);
+
+                    await turnContext.SendActivityAsync("En que te puedo ayudar?", cancellationToken: cancellationToken);
+
+                    //await MainOptionsCard.ToShow(_conversationState., cancellationToken);
+
+                    //return await _dialog.BeginDialogAsync(nameof(RootDialog), rootDialog, cancellationToken);
+
+                    // await WelcomeCard.ToShow(stepContext, cancellationToken);
                 }
             }
+        }
+
+        // Load attachment from embedded resource.
+        private Attachment CreateWelcomeCard()
+        {
+            var cardResourcePath = "MuniBot.Common.json.welcomeCard.json";
+
+            using (var stream = GetType().Assembly.GetManifestResourceStream(cardResourcePath))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    var adaptiveCard = reader.ReadToEnd();
+                    return new Attachment()
+                    {
+                        ContentType = "application/vnd.microsoft.card.adaptive",
+                        Content = JsonConvert.DeserializeObject(adaptiveCard),
+                    };
+                }
+            }
+        }
+
+        private Attachment CreateMenuCard()
+        {
+            var cardResourcePath = "MuniBot.Common.json.menuCard.json";
+
+            using (var stream = GetType().Assembly.GetManifestResourceStream(cardResourcePath))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    var adaptiveCard = reader.ReadToEnd();
+                    return new Attachment()
+                    {
+                        ContentType = "application/vnd.microsoft.card.adaptive",
+                        Content = JsonConvert.DeserializeObject(adaptiveCard),
+                    };
+                }
+            }
+
         }
 
         public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
